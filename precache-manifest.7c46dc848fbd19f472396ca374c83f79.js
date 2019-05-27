0a708014c903b898202424e3b38322473bb63eb0
Michael Handria <michaelhandria@gmail.com>
2019-05-27 00:22:48 -0700

    initial commit

---
+++ precache-manifest.7c46dc848fbd19f472396ca374c83f79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3895b9ae0c90d8b3c14a80244b0fc7df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a2bdfa8f8ab9ab62b1ce",
+    "url": "./static/css/main.8836b7f2.chunk.css"
+  },
+  {
+    "revision": "360a6bda67009eaecf01",
+    "url": "./static/js/2.377f74b5.chunk.js"
+  },
+  {
+    "revision": "a2bdfa8f8ab9ab62b1ce",
+    "url": "./static/js/main.79d166d8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
